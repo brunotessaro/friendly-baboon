e0bfaf9828253843fd44b58e21050e84ecc1dd03
Bruno <bruno.tessaro@gmail.com>
2019-01-09 14:54:17 +0000

    Temperature residual added to the routine accoring to paper calculations, seems write but has to be further tested which can only occur whenthe full code is implemented

--- elementSubRoutine.m
+++ elementSubRoutine.m
@@ -68,15 +68,25 @@ for i=1:size(volElemIdx,1)
                 B = J\[psi.dcsi(ig,:) ; psi.deta(ig,:)];
             end
             
-            M = psi.sf(ig,:)'*rho*Cp*psi.sf(ig,:);
-            Kappa = B'*k*B;
+%%%---------------------- Arrange later ----------------------%%          
+            rho = 1;
+            rho_n = 1;
+            Cp = 1;
+            Cp_n = 1;
+            k = 1;
+            k_n = 1;
+%%%-----------------------------------------------------------%%          
+
+    
+            M = psi.sf(ig,:)'*psi.sf(ig,:);
+            Kappa = B'*B;
             F_Q = M*Q(Te);
             
-            re = re + gWts(ig)*(M*(u(Te) - u_n(Te)) ...
-                                + dt*(1 - alpha)*(Kappa*u_n(Te) - F_Q) ...
-                                + alpha*dt*(Kappa*u(Te) - F_Q))*det(J);     
+            re_u = re_u + gWts(ig)*(M*rho*Cp*(u(Te) - u_n(Te)) ...
+                                + alpha*dt*(Kappa*k*u(Te) - F_Q) ...
+                                + dt*(1-alpha)*(rho*Cp)/(rho_n*Cp_n)*(Kappa*k_n*u_n(Te) - F_Q))*det(J);     
             
-            Ke = Ke + gWts(ig)*(M + alpha*dt*Kappa)*det(J);
+            %Ke = %Ke + gWts(ig)*(M + alpha*dt*Kappa)*det(J);
             
     end 
     
@@ -105,30 +115,27 @@ for i=1:size(bcInfo,2)
             iElem = bcInfo{i}{1}(j);
             [psi, gWts, nElNds, ~] = elementCall(elemType(iElem), [1 0]);
             Te = T(iElem,1:nElNds);
-            Xe = X(Te,:);
-            
+            Xe = X(Te,:);          
             re_u = zeros(size(Te,2),1);
-            re_c = zeros(size(Te,2),1);
-            Ke_uu = zeros(size(Te,2));
-            Ke_cu = zeros(size(Te,2));
-            Ke_uc = zeros(size(Te,2));
-            Ke_cc = zeros(size(Te,2));
             
             % Loop trough element gauss points
             for ig=1:size(gWts,2)
                 J = jacobCalc(elemType(iElem), ig, Xe, psi);
-                re = re + gWts(ig)*(-alpha*dt*psi.sf(ig,:)'*bcInfo{i}{3} ...
-                    -dt*(1-alpha)*psi.sf(ig,:)'*bcInfo{i}{3})*det(J);
+                
+              
+%%%---------------------- Arrange later ----------------------%%          
+            q = bcInfo{i}{3};
+            q_n = bcInfo{i}{3};
+%%%-----------------------------------------------------------%%   
+               
+
+                re_u = re_u + gWts(ig)*(-alpha*dt*psi.sf(ig,:)'*q ...
+                                        -dt*(1-alpha)*(rho*Cp)/(rho_n*Cp_n)*psi.sf(ig,:)'*q_n)*det(J);
+            
             end
             
             % Assembly of residual and tangent matrix
-            r_u(Te) = r_u(Te) + re_u;
-            r_c(Te) = r_c(Te) + re_c;
-            K_uu(Te,Te) = K_uu(Te, Te) + Ke_uu;
-            K_uc(Te,Te) = K_uc(Te, Te) + Ke_uc;
-            K_cu(Te,Te) = K_cu(Te, Te) + Ke_cu;
-            K_cc(Te,Te) = K_cc(Te, Te) + Ke_cc;
-            
+            r_u(Te) = r_u(Te) + re_u;            
         end
         
         % Same procedure for convective boundary
@@ -140,26 +147,29 @@ for i=1:size(bcInfo,2)
             Xe = X(Te,:);
             
             re_u = zeros(size(Te,2),1);
-            re_c = zeros(size(Te,2),1);
             Ke_uu = zeros(size(Te,2));
-            Ke_cu = zeros(size(Te,2));
             Ke_uc = zeros(size(Te,2));
-            Ke_cc = zeros(size(Te,2));
             
             for ig=1:size(gWts,2)
                 J = jacobCalc(elemType(iElem), ig, Xe, psi);
-                re = re + gWts(ig)*(-alpha*dt*psi.sf(ig,:)'*bcInfo{i}{4}*(bcInfo{i}{3} - psi.sf(ig,:)*u(Te)) ...
-                    -dt*(1-alpha)*psi.sf(ig,:)'*bcInfo{i}{4}*(bcInfo{i}{3} - psi.sf(ig,:)*u_n(Te)))*det(J);
-                Ke = Ke + gWts(ig)*(alpha*dt*(psi.sf(ig,:)'*bcInfo{i}{4}*psi.sf(ig,:)))*det(J);
+                
+%%%---------------------- Arrange later ----------------------%%          
+            h = bcInfo{i}{4};
+            h_n = bcInfo{i}{4};
+            u_a = bcInfo{i}{3};
+%%%-----------------------------------------------------------%%  
+                
+                
+                re_u = re_u + gWts(ig)*(-alpha*dt*psi.sf(ig,:)'*h*(u_a - psi.sf(ig,:)*u(Te)) ...
+                                        -dt*(1-alpha)*(rho*Cp)/(rho_n*Cp_n)*psi.sf(ig,:)'*h_n*(u_a - psi.sf(ig,:)*u_n(Te)))*det(J);
+                
+                
+                %Ke = %Ke + gWts(ig)*(alpha*dt*(psi.sf(ig,:)'*bcInfo{i}{4}*psi.sf(ig,:)))*det(J);
             end
             
             r_u(Te) = r_u(Te) + re_u;
-            r_c(Te) = r_c(Te) + re_c;
             K_uu(Te,Te) = K_uu(Te, Te) + Ke_uu;
-            K_uc(Te,Te) = K_uc(Te, Te) + Ke_uc;
-            K_cu(Te,Te) = K_cu(Te, Te) + Ke_cu;
-            K_cc(Te,Te) = K_cc(Te, Te) + Ke_cc;
-            
+            K_uc(Te,Te) = K_uc(Te, Te) + Ke_uc;            
         end
         
         % Same procedure for radiative boundary
@@ -170,27 +180,29 @@ for i=1:size(bcInfo,2)
             Te = T(iElem,1:nElNds);
             Xe = X(Te,:);
             
+%%%---------------------- Arrange later ----------------------%%          
+            eps = bcInfo{i}{4};
+            eps_n = bcInfo{i}{4};
+            u_a = bcInfo{i}{3};
+%%%-----------------------------------------------------------%%  
+            
             re_u = zeros(size(Te,2),1);
-            re_c = zeros(size(Te,2),1);
             Ke_uu = zeros(size(Te,2));
-            Ke_cu = zeros(size(Te,2));
             Ke_uc = zeros(size(Te,2));
-            Ke_cc = zeros(size(Te,2));
             
             for ig=1:size(gWts,2)
                 J = jacobCalc(elemType(iElem), ig, Xe, psi);
-                re = re + gWts(ig)*(-alpha*dt*(psi.sf(ig,:)'*sig*bcInfo{i}{4}*(bcInfo{i}{3}^4 - (psi.sf(ig,:)*u(Te))^4)) ...
-                    -dt*(1-alpha)*psi.sf(ig,:)'*sig*bcInfo{i}{4}*(bcInfo{i}{3}^4 - (psi.sf(ig,:)*u_n(Te))^4))*det(J);
-                Ke = Ke + gWts(ig)*(alpha*dt*(psi.sf(ig,:)'*4*sig*bcInfo{i}{4}*(psi.sf(ig,:)*u(Te))^3*psi.sf(ig,:)))*det(J);
+                
+                re_u = re_u + gWts(ig)*(-alpha*dt*(psi.sf(ig,:)'*sig*eps*(u_a^4 - (psi.sf(ig,:)*u(Te))^4)) ...
+                                        -dt*(1-alpha)*(rho*Cp)/(rho_n*Cp_n)*psi.sf(ig,:)'*sig*eps_n*(u_a^4 - (psi.sf(ig,:)*u_n(Te))^4))*det(J);
+                
+                
+                %Ke = %Ke + gWts(ig)*(alpha*dt*(psi.sf(ig,:)'*4*sig*bcInfo{i}{4}*(psi.sf(ig,:)*u(Te))^3*psi.sf(ig,:)))*det(J);
             end
             
             r_u(Te) = r_u(Te) + re_u;
-            r_c(Te) = r_c(Te) + re_c;
             K_uu(Te,Te) = K_uu(Te, Te) + Ke_uu;
             K_uc(Te,Te) = K_uc(Te, Te) + Ke_uc;
-            K_cu(Te,Te) = K_cu(Te, Te) + Ke_cu;
-            K_cc(Te,Te) = K_cc(Te, Te) + Ke_cc;
-            
         end
     end
 end
